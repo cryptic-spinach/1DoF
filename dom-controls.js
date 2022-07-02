68135d07841a4606ea37abed55b22b2cfb8b2770
Chase Reynolds <chase@Chases-MBP.home>
2022-07-02 17:03:10 -0400

    Import recording controls

--- dom-controls.js
+++ dom-controls.js
@@ -1,3 +1,53 @@
+let startTime;
+let mouseRecording;
+let isRecording = false;
+
+let listen = function(e) {
+    let elapsedTime = Date.now() - startTime;
+    let percentX = parseFloat(e.clientX / window.innerWidth * 100).toFixed(2);
+    let percentY = parseFloat(e.clientY / window.innerHeight * 100).toFixed(2);
+    mouseRecording.push(elapsedTime + "," + percentX + "," + percentY);
+}
+
+function record() {
+    if (isRecording == false) {
+        reInitializeGlobals();
+        let recordButton = document.querySelector("#recordbutton");
+        isRecording = true;
+        recordButton.innerHTML = "Stop Recording";
+        window.addEventListener("mousemove", listen);
+    } 
+    else {
+        let recordButton = document.querySelector("#recordbutton");
+        isRecording = false;
+        recordButton.innerHTML = "Record";
+        window.removeEventListener("mousemove", listen);
+    }
+}
+
+function reInitializeGlobals() {
+    startTime = Date.now();
+    mouseRecording = ["time-in-milliseconds,percent-x,percent-y"];
+}
+
+function download() {
+    const file = new File([mouseRecording.join("\n")], 'mouse-recording.csv', {
+        type: 'text/csv',
+    });
+
+    const link = document.createElement('a');
+    const url = URL.createObjectURL(file);
+
+    link.href = url;
+    link.download = file.name;
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+    window.URL.revokeObjectURL(url);
+}
+
+
 function play() {
     if (sound.isPlaying()) {
         sound.pause();
